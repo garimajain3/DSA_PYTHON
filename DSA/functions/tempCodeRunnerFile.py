8d887b1e2dfbd8657917898cd13c62656cb48f42
Garima jain <50513270+garimajain3@users.noreply.github.com>
2026-03-06 22:21:17 +0530

    Delete DSA/functions directory

--- DSA/functions/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-def prime_function(n):
-    for i in range(2,int(n//2)+1):
-        if(n%2==0):
-            print("Not Prime")
-            break
-    else:
-        print("Prime")
-prime_function(n)
